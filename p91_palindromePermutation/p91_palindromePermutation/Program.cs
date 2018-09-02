5708e4e7edafb28cbb2577b2374701e622ba6998
SirJUST <theoperaking@yahoo.com>
2018-09-02 09:18:03 -0700

    permutation of palindrome

---
+++ p91_palindromePermutation/p91_palindromePermutation/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// checks if a string is a permutation of a palindrome
+
+namespace p91_palindromePermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isPermutation("hello"));
+            Console.WriteLine(isPermutation("1221"));
+            Console.WriteLine(isPermutation("rats live on no evil star"));
+            Console.WriteLine(isPermutation("palindrome"));
+            Console.WriteLine(isPermutation("12:21"));
+            Console.WriteLine(isPermutation("123456654321"));
+            Console.WriteLine(isPermutation("hhheelloooo"));
+            Console.Read();
+        }
+
+        static bool isPermutation(string input)
+        {
+            input = input.Trim(' ').ToLower();
+            char[] inputArray = input.ToCharArray();
+            Array.Sort(inputArray);
+            bool isPermutation = true;
+            if (inputArray.Length % 2 == 0)
+            {
+                for(int i = 0; i <= inputArray.Length - 2; i += 2)
+                {
+                    if (!(inputArray[i] == inputArray[i + 1]))
+                    {
+                        isPermutation = false;
+                        break;
+                    }
+                    else continue;
+                }
+            }
+            else
+            {
+                int numberOfMismatches = 0;
+                bool lastOneTrue = false;
+                for (int i = 0; i < inputArray.Length-1; i++)
+                {
+                    if (numberOfMismatches > 1)
+                    {
+                        isPermutation = false;
+                        break;
+                    }
+                    if (lastOneTrue)
+                    {
+                        lastOneTrue = false;
+                        continue;
+                    }
+
+                    if (inputArray[i] == inputArray[i + 1])
+                    {
+                        lastOneTrue = true;
+                        continue;
+                    }
+                    else
+                    {
+                        numberOfMismatches += 1;
+                        continue;
+                    }
+                }
+            }
+            return isPermutation;
+        }
+    }
+}
