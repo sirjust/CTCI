75f5732d0aa28318e86e0ff49dcaee2f5e0225f0
SirJUST <theoperaking@yahoo.com>
2019-02-14 18:05:16 -0500

    string compression algorithm

---
+++ StringCompression/StringCompression/Compress.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCompression
+{
+    public class Compress
+    {
+        public string CompressString(string input)
+        {
+            char[] inputArray = input.ToCharArray();
+            if(inputArray.Length == 1 || inputArray.Length == 0)
+            {
+                return input;
+            }
+            List<char> compressedList = new List<char>();
+            int consecutiveLetters = 1;
+            for(int i = 1; i< inputArray.Length; i++)
+            {
+                if (i == inputArray.Length - 1)
+                {
+                    if (inputArray[i] == inputArray[i - 1])
+                    {
+                        consecutiveLetters++;
+                    }
+                    compressedList.Add(inputArray[i]);
+                    if (consecutiveLetters > 9)
+                    {
+                        char[] numberArray = consecutiveLetters.ToString().ToCharArray();
+                        foreach (char c in numberArray)
+                        {
+                            compressedList.Add(c);
+                        }
+                    }
+                    else
+                    {
+                        compressedList.Add(Convert.ToChar(consecutiveLetters.ToString()));
+                    }
+                    break;
+                }
+                if (inputArray[i] == inputArray[i - 1])
+                {
+                    consecutiveLetters++;
+                }
+                else
+                {
+                    compressedList.Add(inputArray[i - 1]);
+                    if (consecutiveLetters > 9)
+                    {
+                        char[] numberArray = consecutiveLetters.ToString().ToCharArray();
+                        foreach(char c in numberArray)
+                        {
+                            compressedList.Add(c);
+                        }
+                    }
+                    else
+                    {
+                        compressedList.Add(Convert.ToChar(consecutiveLetters.ToString()));
+                    }
+                    consecutiveLetters = 1;
+                }
+            }
+            string myReturn = String.Join("", compressedList);
+            return myReturn;
+        }
+    }
+}
---
+++ StringCompression/StringCompression/Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCompression
+{
+    [TestFixture]
+    public class Test
+    {
+        Compress compress = new Compress();
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("a1b4c2", compress.CompressString("abbbbcc"));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("a10", compress.CompressString("aaaaaaaaaa"));
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual("a5b6", compress.CompressString("aaaaabbbbbb"));
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual("a", compress.CompressString("a"));
+        }
+        [Test]
+        public void Test5()
+        {
+            Assert.AreEqual("", compress.CompressString(""));
+        }
+    }
+}
