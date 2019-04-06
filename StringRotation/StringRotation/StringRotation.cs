4430ffe7dcebb7d6c00be7a7d817a74119ec8659
Justin Moore <Justin.Moore@purplebricks.com>
2019-04-06 13:21:19 -0400

    working implementation of stringRotation algorithm with tests

---
+++ StringRotation/StringRotation/StringRotation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRotation
+{
+    public class StringRotation
+    {
+        public bool stringRotation(string original, string itemToBeChecked)
+        {
+            if (original.Length != itemToBeChecked.Length)
+            {
+                return false;
+            }
+
+            if (original == itemToBeChecked)
+            {
+                return true;
+            }
+
+            var firstLetter = original[0];
+            // this will throw an exception if there isn't a corresponding index
+            var correspondingIndex = itemToBeChecked.IndexOf(firstLetter);
+            var j = 0;
+            for (var i = correspondingIndex; i < itemToBeChecked.Length; i++)
+            {
+                if (itemToBeChecked[i] != original[j])
+                {
+                    return false;
+                }
+
+                j++;
+            }
+            return IsSubString(original, itemToBeChecked.Remove(correspondingIndex));
+        }
+
+        public bool IsSubString(string firstString, string stringToBeChecked)
+        {
+            if (firstString == null || stringToBeChecked == null)
+            {
+                return false;
+            }
+            var firstLetter = stringToBeChecked[0];
+            var correspondingIndex = firstString.IndexOf(firstLetter);
+            var j = 0;
+            for (int i = correspondingIndex; i<stringToBeChecked.Length; i++)
+            {
+                if (firstString[i] != stringToBeChecked[j])
+                {
+                    return false;
+                }
+                j++;
+            }
+            return true;
+        }
+    }
+}
---
+++ StringRotation/StringRotation/Test.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringRotation
+{
+    public class Test
+    {
+        [Fact]
+        public void IsSubstringShouldReturnTrue_WhenFirstStringHello_AndStringToBeCheckedHell()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.IsSubString("hello", "hell");
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsSubStringShouldReturnTrue_WhenFirstStringHelloWorld_AndStringToBeCheckedoWo()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.IsSubString("HelloWorld", "oWo");
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsSubStringShouldReturnFalse_WhenStringToBeCheckedNull()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.IsSubString("HelloWorld", null);
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsSubStringShouldReturnFalse_WhenFirstStringNull()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.IsSubString(null, "oWo");
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void stringRotationShouldReturnTrue_WhenOriginalWaterbottle_AndItemToBeCheckederbottleWat()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.stringRotation("waterbottle", "erbottlewat");
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void stringRotationShouldReturnTrue_WhenOriginalHello_AndItemToBeCheckedoHell()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.stringRotation("Hello", "oHell");
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void stringRotationShouldReturnTrue_WhenOriginalcomputer_AndItemToBeCheckedutercomp()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.stringRotation("computer", "utercomp");
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void stringRotationShouldReturnTrue_WhenOriginalutercomp_AndItemToBeCheckedcomputer()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.stringRotation("utercomp", "computer");
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void stringRotationShouldReturnTrue_WhenOriginalcomputer_AndItemToBeCheckedcomputer()
+        {
+            //Arrange
+            var stringRotation = new StringRotation();
+
+            //Act
+            var result = stringRotation.stringRotation("computer", "computer");
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
